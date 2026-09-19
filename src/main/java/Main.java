08a6ceb8f84aab951219d2544d5c1823c857a817
oscar <oscar@OscarDVN>
2026-09-18 20:50:32 -0500

    cambios de programa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -61,7 +61,7 @@ public class Main {
                                 System.out.print("Nombre completo: ");
                                 nombre = scanner.nextLine().trim();
                                 if (!Cliente.esNombreValido(nombre)) {
-                                    System.out.println("[ERROR] El nombre debe contener letras y puede incluir espacios o apóstrofes.");
+                                    System.out.println("[ERROR] El nombre debe contener letras y puede incluir espacios o apóstrofos.");
                                 }
                             } while (!Cliente.esNombreValido(nombre));
 
